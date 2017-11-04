20b77c31dd7c6573d8b93226ba68ed9f096549f8
Tejusvi Srivastva <srivastva.tejusvi@gmail.com>
2017-11-04 17:59:50 +0530

    Changing files into php file
    removing common header footer content and adding php's include instead.

---
+++ about.php
@@ -0,0 +1,43 @@
+<?php 
+$PageTitle = 'About Us';
+$breadcrumbs = '<li><a href="index.html"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home</a> <i>/</i></li>';
+$breadcrumbs .=	'<li>'.$PageTitle.'</li>';
+include_once('./shared/header.php');
+?>
+<!-- about -->
+	<div class="about">
+		<div class="container">	
+			<div class="w3ls_about_grids">
+				<div class="col-md-6 w3ls_about_grid_left">
+					<p>Duis aute irure dolor in reprehenderit in voluptate velit esse 
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+						cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+						anim id est laborum.</p>
+					<div class="col-xs-2 w3ls_about_grid_left1">
+						<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>
+					</div>
+					<div class="col-xs-10 w3ls_about_grid_left2">
+						<p>Sunt in culpa qui officia deserunt mollit 
+							anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse 
+						cillum dolore eu fugiat nulla pariatur.</p>
+					</div>
+					<div class="clearfix"> </div>
+					<div class="col-xs-2 w3ls_about_grid_left1">
+						<span class="glyphicon glyphicon-flash" aria-hidden="true"></span>
+					</div>
+					<div class="col-xs-10 w3ls_about_grid_left2">
+						<p>Sunt in culpa qui officia deserunt mollit 
+							anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse 
+						cillum dolore eu fugiat nulla pariatur.</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="col-md-6 w3ls_about_grid_right">
+					<img src="images/52.jpg" alt=" " class="img-responsive" />
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<!-- //about -->
+	<?php include_once('./shared/footer.php');
